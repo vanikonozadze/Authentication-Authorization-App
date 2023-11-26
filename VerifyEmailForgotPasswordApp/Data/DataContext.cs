b5099343479307a0d8049218328183999a460bee
Vaniko Nozadze <ivane.nozadze.1@btu.edu.ge>
2023-11-26 23:53:24 +0400

    Add project files.

---
+++ VerifyEmailForgotPasswordApp/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿namespace VerifyEmailForgotPasswordApp.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server=DESKTOP-PA4SOKH\\SQLEXPRESS;database=userdb;Trusted_Connection=true");
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ VerifyEmailForgotPasswordApp/Models/UserLoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VerifyEmailForgotPasswordApp.Models
+{
+    public class UserLoginRequest
+    {
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
